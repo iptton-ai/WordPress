2cf1f5db71be3e33dfb73e713b313c3427320844
iptton <iptton@gmail.com>
2025-11-10 00:22:34 +0800

    up

--- wp-includes/feed.php
+++ wp-includes/feed.php
@@ -599,11 +599,7 @@ function prep_atom_text_construct( $data ) {
 	$parser = xml_parser_create();
 	xml_parse( $parser, '<div>' . $data . '</div>', true );
 	$code = xml_get_error_code( $parser );
-
-	if ( PHP_VERSION_ID < 80000 ) { // xml_parser_free() has no effect as of PHP 8.0.
-		xml_parser_free( $parser );
-	}
-
+	xml_parser_free( $parser );
 	unset( $parser );
 
 	if ( ! $code ) {
@@ -812,8 +808,7 @@ function fetch_feed( $url ) {
 
 	$feed = new SimplePie\SimplePie();
 
-	$feed->get_registry()->register( SimplePie\Sanitize::class, 'WP_SimplePie_Sanitize_KSES', true );
-
+	$feed->set_sanitize_class( 'WP_SimplePie_Sanitize_KSES' );
 	/*
 	 * We must manually overwrite $feed->sanitize because SimplePie's constructor
 	 * sets it before we have a chance to set the sanitization class.
@@ -830,7 +825,7 @@ function fetch_feed( $url ) {
 		$feed->set_cache_class( 'WP_Feed_Cache' );
 	}
 
-	$feed->get_registry()->register( SimplePie\File::class, 'WP_SimplePie_File', true );
+	$feed->set_file_class( 'WP_SimplePie_File' );
 
 	$feed->set_feed_url( $url );
 	/** This filter is documented in wp-includes/class-wp-feed-cache-transient.php */
